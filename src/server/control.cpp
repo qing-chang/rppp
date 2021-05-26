c3d084436118622a5b02a37ee7f95d767e9f956d
changqing <cq@ippp.net.cn>
2021-05-26 13:45:28 +0800

    a

--- src/server/control.cpp
+++ src/server/control.cpp
@@ -46,15 +46,19 @@ std::task<> Control::controlCoRoutine()
         {
         case msgType::Auth :
             {
-                std::cout <<"auth_user:"<< std::static_pointer_cast<auth>(msg.msg_)->user << std::endl;
-                std::cout <<"auth_password:"<<  std::static_pointer_cast<auth>(msg.msg_)->password << std::endl;
+                // std::cout <<"auth_user:"<< std::static_pointer_cast<auth>(msg.msg_)->user << std::endl;
+                // std::cout <<"auth_password:"<<  std::static_pointer_cast<auth>(msg.msg_)->password << std::endl;
                 auto got = (config.conf)->user.find(std::static_pointer_cast<auth>(msg.msg_)->user);
                 if ((got != (config.conf)->user.end()) && 
                     (got->second == std::static_pointer_cast<auth>(msg.msg_)->password))
                 {
+                    //认证成功
+                    std::cout <<"/认证成功"<< std::endl;
                     initControl();
                 } else
                 {
+                    //认证失败
+                    std::cout <<"/认证失败"<< std::endl;
                     co_return;
                 }
                 break;
