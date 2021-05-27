ace80663ea2e873358173114350f86d5b68862d2
changqing <cq@ippp.net.cn>
2021-05-27 09:20:02 +0800

    q

--- src/client/model.cpp
+++ src/client/model.cpp
@@ -12,7 +12,6 @@ void ClientModel::Run()
 
 std::task<> ClientModel::control()
 {
-    std::cout<<"control"<<"\n";
     //---------------------------------------------------------------------------------------------------------
     socket =  std::shared_ptr<Socket>(new Socket{io_context, 0});
     int c = co_await socket->connect(config.conf->serverAddr, config.conf->serverPort);
@@ -23,20 +22,33 @@ std::task<> ClientModel::control()
     {
         std::cout << "connect succeed" << std::endl;
     }
-    //---------------------------------------发生auth-----------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------
     char sndBuff[SND_BUFF_SIZE];
+    char rcvBuff[RCV_BUFF_SIZE];
     Msg msg;
+    //---------------------------------------发生auth-----------------------------------------------------------
     msg.type = msgType::Auth;
     std::shared_ptr<auth> auth_(new auth{(config.conf)->userName, (config.conf)->password});
     msg.msg_ = std::static_pointer_cast<void>(auth_);
     co_await Msg::writeMsg(socket, sndBuff, &msg);
     //---------------------------------------接收auth-----------------------------------------------------------
-    char rcvBuff[RCV_BUFF_SIZE];
     ssize_t nbRcved = 0;
-    Msg msg;
     while(true)
     {
         co_await Msg::readMsg(socket, rcvBuff, &nbRcved, &msg);
-
+        if(msg.type == msgType::AuthResp)
+        {
+            std::cout << "认证成功........." << std::endl;
+            break;
+        }
     }
+    //---------------------------------------发送ReqTunnel-------------------------------------------------------
+    msg.type = msgType::ReqTunnel;
+    std::shared_ptr<reqTunnel> reqTunnel_(new reqTunnel);
+    msg.msg_ = std::static_pointer_cast<void>(reqTunnel_);
+    // for()
+    // {
+
+    //     co_await Msg::writeMsg(socket, sndBuff, &msg);
+    // }
 }
