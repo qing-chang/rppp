1a8ee43e0d1fd38cf952d2b09fc130879296464d
changqing <cq@ippp.net.cn>
2021-05-31 15:26:14 +0800

    q

--- src/server/tunnel.cpp
+++ src/server/tunnel.cpp
@@ -8,7 +8,7 @@ std::task<> Tunnel::NewTunnel()
     char sndBuff[SND_BUFF_SIZE];
     Msg msg;
     msg.type = msgType::NewTunnel;
-    std::shared_ptr<newTunnel> newTunnel_(new newTunnel{"newTunnel"});
+    std::shared_ptr<newTunnel> newTunnel_(new newTunnel{std::to_string(remotePort)});
     msg.msg_ = std::static_pointer_cast<void>(newTunnel_);
     co_await _msg_::writeMsg(control->socket, sndBuff, &msg);
     //------------------------------开始监听用户链接---------------------------------------------------------
