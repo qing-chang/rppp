bc04b1fbc5c6f64f553a87cd22870b0e32a6d40e
changqing <cq@ippp.net.cn>
2021-06-03 08:46:50 +0800

    q

--- mis/TCP_server.c
+++ mis/TCP_server.c
@@ -11,19 +11,18 @@ int main()
      struct sockaddr_in server_address,client_address;
      server_fd = socket(AF_INET,SOCK_STREAM, 0);
      server_address.sin_family = AF_INET;
-     server_address.sin_port = htons(80);
+     server_address.sin_port = htons(8888);
      server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-     bind(server_fd,(struct sockaddr *)&server_address, sizeof(struct sockaddr_in));
-     listen(server_fd,5);
+     bind(server_fd,(struct sockaddr *)&server_address, sizeof(server_address));
+     listen(server_fd,50);
      printf("waiting...\n");
-     client_len=sizeof(struct sockaddr_in);
+     client_len=sizeof(client_address);
      client_fd = accept(server_fd, (struct sockaddr*)&client_address, (socklen_t *__restrict)&client_len);
      while(1)
      {
-          char ch;
-          read(client_fd,&ch,1);
-          // ch++;
-          write(client_fd,&ch,1);
+          char ch[100];
+          int res = read(client_fd,ch,100);
+          write(client_fd,ch,res);
      }
      close(client_fd);
      return 0;
--- src/client/model.cpp
+++ src/client/model.cpp
@@ -16,12 +16,12 @@ std::task<> ClientModel::control()
     //--------------------------------------------------------------------------------------------------------
     socket =  std::shared_ptr<Socket>(new Socket{io_context, 0});
     int c = co_await socket->connect(config.conf->serverAddr, config.conf->serverPort);
-    if(c != 0)
-    {
-        std::cout << "connect fail" << std::endl;
-    }else
+    if(c == 0)
     {
         std::cout << "connect succeed" << std::endl;
+    }else
+    {
+        std::cout << "connect fail" << std::endl;
     }
     //--------------------------------------------------------------------------------------------------------
     char sndBuff[SND_BUFF_SIZE];
